54db280f2c8d32775a908a0d4653a04c2fd32660
Gabriel N <gabrielnaldi.castro@gmail.com>
2023-07-31 21:20:01 -0300

    style: fix z-index

---
+++ src/app/memories/page.tsx
@@ -0,0 +1,3 @@
+export default function MemoriesPage() {
+  return <div>oi</div>
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Link from 'next/link'
 import NlwSpacetimeLogo from './components/nlw-spacetime-logo'
 
 export default function Home(): JSX.Element {
   return (
     <main className="relative grid min-h-screen items-end overflow-hidden bg-gray-700 px-8 py-10">
-      <div className="flex items-center justify-center ">
+      <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700/75 blur-194" />
+      <div className="z-10 flex items-center justify-center">
         <div className="flex flex-col items-center space-y-6">
           <NlwSpacetimeLogo />
           <div className="space-y-1 text-center">
@@ -15,18 +17,16 @@ export default function Home(): JSX.Element {
               quiser) com o mundo!
             </p>
           </div>
-          <a
-            href=""
-            className="inline-block rounded-full bg-green-500 px-5 py-3 font-baiJamJuree text-gray-900 transition-colors duration-300 hover:bg-green-700"
-          >
-            COMEÇAR A CADASTRAR
-          </a>
+          <Link href="/memories">
+            <span className="inline-block rounded-full bg-green-500 px-5 py-3 font-baiJamJuree text-gray-900 transition-colors duration-300 hover:bg-green-700">
+              COMEÇAR A CADASTRAR
+            </span>
+          </Link>
         </div>
       </div>
       <div className="text-center">
         <p className="leading-relaxed">Feito com 💜 no NLW da Rocketseat</p>
       </div>
-      <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700/75 blur-194" />
     </main>
   )
 }
